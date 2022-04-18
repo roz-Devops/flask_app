8402db5a62425967079ca03c7a3409751c709e94
roz-Devops <rozanaiv4@gmail.com>
2022-04-18 19:33:49 +0300

    prometheus exporter

--- app/server.py
+++ app/server.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask import request, Response
 from flask_healthz import healthz
 from flask_healthz import HealthError
+from prometheus_flask_exporter import PrometheusMetrics
 import sys
 
 app = Flask(__name__)
 port = sys.argv[1];
 app.register_blueprint(healthz, url_prefix="/healthz")
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='flask_app_ubuntu:latest')
+
 
 def printok():
     print("It's Alive and Works smoothly!")
